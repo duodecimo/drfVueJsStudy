0840535d72c808ba444c78ad8ed0e69e2e42a7aa
duodecimo.fernandes <duodecimo.fernandes@virtualinteractions.com.br>
2020-10-30 19:27:42 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # drfVueJsStudy
 Projects to study django rest framework and vuejs
+
+## Environment
+Python django rest framework
+Vue.js
+
+Visual Studio Code
+Ubuntu Linux 20.04 LTS
+
---
+++ projects/prj001_oneClass/oneApp/views.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Person
+from .serializers import PersonSerializer
+
+# Create your views here.
+
+
+class PersonViewSet(ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer

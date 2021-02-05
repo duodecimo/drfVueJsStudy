a0758b3597254d61dd87f6f31a0fc05e8f7ada4b
duodecimo.fernandes <duodecimo.fernandes@virtualinteractions.com.br>
2021-02-05 15:17:16 -0300

    ajustes no modelo

--- projects/pratudo_saude/test/widget_test.dart
+++ projects/pratudo_saude/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pratudo_saude/ex_main.txt';
+import 'package:pratudo_saude/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
